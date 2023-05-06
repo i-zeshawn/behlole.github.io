b0487bdaa75708af50622fb45510abf8fac577da
i-zeshawn <zeeshahmed.dev@gmail.com>
2023-05-06 18:34:29 -0400

    implemented technology section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {BallCanvas} from "./canvas/index.js";
+import SectionWrapper from "../hoc/index.js";
+import {technologies} from "../constants/index.js";
 
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
+    return (
+        <div className={"flex flex-row flex-wrap justify-center gap-10"}>
+            {technologies.map((technology, index) => {
+                return (<div className={"w-28 h-28"} key={index}>
+                    <BallCanvas icon={technology.icon}></BallCanvas>
+                </div>);
+            })}</div>
+    )
 }
 
 export default Tech
