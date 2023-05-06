84fc4d799fb2bc27f4449df289e280ce3683c8fd
i-zeshawn <zeeshahmed.dev@gmail.com>
2023-05-06 15:57:32 -0400

    optimized navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React, {useState} from 'react'
 import {styles} from "../style.js";
 import {logo, menu, close} from "../assets/index.js";
 import {Link} from "react-router-dom";
+import {navLinks} from "../constants/index.js";
 
 const Navbar = () => {
     const [active, setActive] = useState("");
+    const [toggle, setToggle] = useState(false);
     return (
         <nav
             className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -19,10 +21,32 @@ const Navbar = () => {
                     }}
                 >
                     <img src={logo} alt={"logo"} className={"w-9 h-9 object-contain"}/>
-                    <p className={"text-white text-[18px] font-bold cursor-pointer"}>Behlole
-                        <span className={"sm:block-hidden"}> | Full-Stack Developer</span>
+                    <p className={"text-white text-[18px] font-bold cursor-pointer"}>M Behlole Aqil
+                        <span className={"sm:block hidden"}> | Full-Stack Developer</span>
                     </p>
                 </Link>
+                <ul className={"list-none hidden sm:flex flex-row gap-10"}>
+                    {navLinks.map((singleNavLink) => (
+                        <li
+                            key={singleNavLink.id}
+                            className={`
+                            ${active === singleNavLink.title ? 'text-white' : 'text-secondary'}
+                            hover:text-white text-[18px] cursor-pointer`}
+                            onClick={() => setActive(singleNavLink.title)}
+                        >
+                            <a href={`#${singleNavLink.id}`}>{singleNavLink.title}</a>
+                        </li>
+                    ))
+                    }
+                </ul>
+                <div className={"sm:hidden flex flex-1 justify-end items-center"}>
+                    <img
+                        src={menu}
+                        alt={"menu"}
+                        className={"w-[28px] h-[28px] object-contain cursor-pointer"}
+                        onClick={() => setToggle(!toggle)}
+                    />
+                </div>
             </div>
         </nav>
     );
