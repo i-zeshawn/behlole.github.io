d3d19b85e5e58181cbc3898fd4a4f4a2716bb0a8
i-zeshawn <zeeshahmed.dev@gmail.com>
2023-05-06 19:07:03 -0400

    implemented final earth, stars and contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,9 @@ import {EarthCanvas} from "./canvas";
 import {slideIn} from "../utils/motion";
 import SectionWrapper from "../hoc/index.js";
 import {styles} from "../style.js";
-
+//template_zu7gxsl
+// service_kfy6s4m
+// aFCNmuREqibzGvgcg
 const Contact = () => {
     const formRef = useRef();
     const [form, setForm] = useState({
@@ -33,8 +35,8 @@ const Contact = () => {
 
         emailjs
             .send(
-                import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-                import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+                "service_kfy6s4m",
+                "template_zu7gxsl",
                 {
                     from_name: form.name,
                     to_name: "M Behlole Aqil",
@@ -42,7 +44,7 @@ const Contact = () => {
                     to_email: "behloleaqil@gmail.com",
                     message: form.message,
                 },
-                import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+                "aFCNmuREqibzGvgcg"
             )
             .then(
                 () => {
