05e334c3410bfe08b1baf61a7da775de49f7a20f
i-zeshawn <zeeshahmed.dev@gmail.com>
2023-05-06 16:27:28 -0400

    completed Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import {motion} from "framer-motion";
+import {styles} from "../style.js";
+import {ComputersCanvas} from "./canvas/index.js";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <section className={"relative w-full h-screen mx-auto"}>
+            <div
+                className={`${styles.paddingX}
+                 absolute inset-0 top-[120px]
+                  max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+                <div className={"flex flex-col justify-center items-center mt-5"}>
+                    <div className={"w-5 h-5 rounded-full bg-[#915eff]"}/>
+                    <div className={"w-1 sm:h-80 h-40 violet-gradient"}/>
+                </div>
+                <div>
+                    <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span
+                        className={"text-[#915eff]"}>M Behlole Aqil</span></h1>
+                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                        Delivering high-quality full-stack <br className={"sm:block hidden"}/> solutions with optimal
+                        performance.
+                    </p>
+                </div>
+            </div>
+            <ComputersCanvas/>
+        </section>
+    )
 }
 
 export default Hero
